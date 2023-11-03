4785405cdaa98c677fa4a6d899227d2ccc5064bd
akring <akio@mail.com>
2023-11-03 16:30:53 +0000

    modified

--- lib/CyclopsStack.ts
+++ lib/CyclopsStack.ts
@@ -1,35 +1,57 @@
-import { CfnOutput, Fn, RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
-import { CachePolicy, Distribution, ViewerProtocolPolicy } from "aws-cdk-lib/aws-cloudfront";
+import { CfnOutput, Fn, RemovalPolicy, ScopedAws, SecretValue, Stack, StackProps } from "aws-cdk-lib";
+import { CachePolicy, Distribution, OriginRequestPolicy, ViewerProtocolPolicy } from "aws-cdk-lib/aws-cloudfront";
 import { HttpOrigin, S3Origin } from "aws-cdk-lib/aws-cloudfront-origins";
 import { Mfa, OAuthScope, UserPool } from "aws-cdk-lib/aws-cognito";
+import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
 import { Architecture, FunctionUrlAuthType, Runtime } from "aws-cdk-lib/aws-lambda";
-import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { NodejsFunction, OutputFormat } from "aws-cdk-lib/aws-lambda-nodejs";
 import { BlockPublicAccess, Bucket } from "aws-cdk-lib/aws-s3";
 import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
+import { Secret } from "aws-cdk-lib/aws-secretsmanager";
 import { Construct } from "constructs";
 
 export class CyclopsStack extends Stack {
     constructor(scope: Construct, id: string, props: StackProps) {
         super(scope, id, props);
+        const { region, accountId } = new ScopedAws(this);
 
         const server = new NodejsFunction(this, 'Server', {
             entry: `${__dirname}/../webapp/.output/server/index.mjs`,
             handler: 'handler',
             runtime: Runtime.NODEJS_LATEST,
+            memorySize: 256,
             architecture: Architecture.ARM_64,
+            bundling: {
+                banner: "import { createRequire } from 'module';const require = createRequire(import.meta.url);",
+                format: OutputFormat.ESM,
+            },
         });
         const appEndpoint = server.addFunctionUrl({
             authType: FunctionUrlAuthType.NONE,
         });
 
+        const authSecretName = `${server.functionName}-Secrets`;
         const authorizer = new NodejsFunction(this, 'Authorizer', {
             entry: `${__dirname}/IdpResponseHandler.ts`,
             runtime: Runtime.NODEJS_LATEST,
+            memorySize: 256,
             architecture: Architecture.ARM_64,
+            environment: {
+                AUTH_SECRET_NAME: authSecretName,
+            },
+            bundling: {
+                banner: "import { createRequire } from 'module';const require = createRequire(import.meta.url);",
+                format: OutputFormat.ESM,
+            },
         });
         const authEndpoint = authorizer.addFunctionUrl({
             authType: FunctionUrlAuthType.NONE,
         });
+        authorizer.addToRolePolicy(new PolicyStatement({
+            effect: Effect.ALLOW,
+            actions: ['secretsmanager:DescribeSecret', 'secretsmanager:GetSecretValue'],
+            resources: [`arn:aws:secretsmanager:${region}:${accountId}:secret:${authSecretName}-*`],
+        }));
 
         const bucket = new Bucket(this, 'Bucket', {
             publicReadAccess: false,
@@ -48,17 +70,20 @@ export class CyclopsStack extends Stack {
                 origin: new HttpOrigin(Fn.parseDomainName(appEndpoint.url)),
                 viewerProtocolPolicy: ViewerProtocolPolicy.HTTPS_ONLY,
                 cachePolicy: CachePolicy.CACHING_DISABLED,
+                originRequestPolicy: OriginRequestPolicy.ALL_VIEWER_EXCEPT_HOST_HEADER,
             },
             additionalBehaviors: {
                 "/_nuxt/*": {
                     origin: new S3Origin(bucket, { originPath: 'public' }),
                     viewerProtocolPolicy: ViewerProtocolPolicy.HTTPS_ONLY,
                     cachePolicy: CachePolicy.CACHING_DISABLED,
+                    originRequestPolicy: OriginRequestPolicy.ALL_VIEWER_EXCEPT_HOST_HEADER,
                 },
                 "/oauth2/idpresponse": {
                     origin: new HttpOrigin(Fn.parseDomainName(authEndpoint.url)),
                     viewerProtocolPolicy: ViewerProtocolPolicy.HTTPS_ONLY,
                     cachePolicy: CachePolicy.CACHING_DISABLED,
+                    originRequestPolicy: OriginRequestPolicy.ALL_VIEWER_EXCEPT_HOST_HEADER,
                 }
             }
         });
@@ -75,10 +100,20 @@ export class CyclopsStack extends Stack {
             cognitoDomain: { domainPrefix: Fn.select(0, Fn.split('.', distribution.domainName)) },
         });
         const client = userPool.addClient('Client', {
-            generateSecret: false,
+            generateSecret: true,
             oAuth: {
                 callbackUrls: [authUrl],
                 scopes: [OAuthScope.OPENID],
+            }
+        });
+
+        const secret = new Secret(authorizer, 'Secrets', {
+            secretName: authSecretName,
+            secretObjectValue: {
+                tokenEndpoint: SecretValue.unsafePlainText(`${authDomain.baseUrl()}/oauth2/token`),
+                userPoolClientId: SecretValue.unsafePlainText(client.userPoolClientId),
+                userPoolClientSecret: client.userPoolClientSecret,
+                distributionDomain: SecretValue.unsafePlainText(distribution.domainName),
             }
         });
 
--- lib/IdpResponseHandler.ts
+++ lib/IdpResponseHandler.ts
@@ -1,6 +1,38 @@
-import { APIGatewayProxyHandler } from "aws-lambda";
+import { Handler } from "aws-lambda";
+import * as AWS from 'aws-sdk';
 
-export const handler: APIGatewayProxyHandler = async (event, context) => {
+const secretName = process.env.AUTH_SECRET_NAME!;
+const secretsManager = new AWS.SecretsManager();
+const secretString = (await (secretsManager.getSecretValue({ SecretId: secretName }).promise())).SecretString!;
+const secretJson = JSON.parse(secretString);
+const { userPoolClientId, userPoolClientSecret, distributionDomain, tokenEndpoint } = secretJson;
+const basicAuth = Buffer.from(`${userPoolClientId}:${userPoolClientSecret}`).toString('base64');
+
+export const handler: Handler = async (event, context) => {
+
+    const headers = {
+        'Authorization': `Basic ${basicAuth}`,
+        'Content-Type': 'application/x-www-form-urlencoded',
+    };
+
+    const requestBody = {
+        grant_type: 'authorization_code',
+        client_id: userPoolClientId,
+        client_secret: userPoolClientSecret,
+        redirect_uri: `https://${distributionDomain}${event.rawPath}`,
+        code: event.queryStringParameters.code,
+    } as { [key: string]: string };
+    const encodedForm = Object.keys(requestBody)
+        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(requestBody[key])}`)
+        .join('&');
+
+    const { id_token, access_token, refresh_token, expires_in, token_type } = JSON.parse(
+        await fetch(tokenEndpoint, {
+            method: 'POST', headers, body: encodedForm,
+        }).then(res => res.text())
+    );
+    console.log({ id_token, access_token, refresh_token, expires_in, token_type })
+
     return {
         statusCode: 302,
         headers: {
