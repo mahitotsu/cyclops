ace347ff62c989945f08f988af9dd33ae8a6303e
akring <akio@mail.com>
2023-11-07 15:38:05 +0000

    modified

--- infra/lib/CyclopsStack.ts
+++ infra/lib/CyclopsStack.ts
@@ -1,18 +1,49 @@
-import { Stack, StackProps } from "aws-cdk-lib";
-import { Vpc } from "aws-cdk-lib/aws-ec2";
-import { Cluster, ContainerImage, FargateTaskDefinition } from "aws-cdk-lib/aws-ecs";
+import { CfnOutput, DockerImage, Duration, RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
+import { Code, Function, FunctionUrlAuthType, Runtime } from "aws-cdk-lib/aws-lambda";
+import { LogGroup, RetentionDays } from "aws-cdk-lib/aws-logs";
 import { Construct } from "constructs";
+import * as os from 'os';
 
 export class CyclopsStack extends Stack {
     constructor(scope: Construct, id: string, props: StackProps) {
         super(scope, id, props);
 
-        const vpc = Vpc.fromLookup(this, 'Vpc', { vpcId: 'vpc-0316bba888afa43c5' });
-        const cluster = Cluster.fromClusterAttributes(vpc, 'Cluster', { vpc, clusterName: 'default' });
-
-        const task = new FargateTaskDefinition(this, 'TaskDefinition');
-        const main = task.addContainer('main', {
-            image: ContainerImage.fromRegistry('webapp:0.0.1-SNAPSHOT'),
+        const webapp = new Function(this, 'Webapp', {
+            runtime: Runtime.JAVA_17,
+            code: Code.fromAsset(`${__dirname}/../../webapp`, {
+                bundling: {
+                    image: DockerImage.fromBuild(`${__dirname}/../builder`),
+                    volumes: [{
+                        hostPath: `${os.homedir()}/.m2`, containerPath: '/var/maven/.m2'
+                    }, {
+                        hostPath: `${os.homedir()}/.npm`, containerPath: '/.npm'
+                    }],
+                    environment: {
+                        MAVEN_CONFIG: '/var/maven/.m2',
+                    },
+                    command: ['bash', '-c', [
+                        'mvn clean -Duser.home=/var/maven',
+                        'npm --prefix src/main/nuxt3 run build',
+                        'mkdir -p ./target/classes/public',
+                        'cp -R ./src/main/nuxt3/dist/* ./target/classes/public/',
+                        'mvn package -DfinalName=webapp -Duser.home=/var/maven',
+                        'cp ./target/webapp.jar /asset-output'
+                    ].join(' && ')],
+                }
+            }),
+            handler: 'com.mahitotsu.cyclops.webapp.Handler::handleRequest',
+            memorySize: 256,
+            timeout: Duration.seconds(10),
         });
+        new LogGroup(webapp, 'LogGroup', {
+            logGroupName: `/aws/lambda/${webapp.functionName}`,
+            retention: RetentionDays.ONE_DAY,
+            removalPolicy: RemovalPolicy.DESTROY,
+        });
+        const endpoint = webapp.addFunctionUrl({
+            authType: FunctionUrlAuthType.NONE,
+        });
+
+        new CfnOutput(this, 'WebappEndpointUrl', { value: endpoint.url });
     }
 }
---
+++ webapp/src/main/java/com/mahitotsu/cyclops/webapp/Handler.java
@@ -0,0 +1,29 @@
+package com.mahitotsu.cyclops.webapp;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import com.amazonaws.serverless.exceptions.ContainerInitializationException;
+import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.model.HttpApiV2ProxyRequest;
+import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+
+public class Handler implements RequestStreamHandler {
+
+    private static SpringBootLambdaContainerHandler<HttpApiV2ProxyRequest, AwsProxyResponse> handler;
+    static {
+        try {
+            handler = SpringBootLambdaContainerHandler.getHttpApiV2ProxyHandler(Main.class);
+        } catch (ContainerInitializationException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
+        handler.proxyStream(input, output, context);
+    }
+}
