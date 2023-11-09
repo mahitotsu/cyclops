9add53310594095d14a036f0aa6f8e88bd9b5dfe
akring <akio@mail.com>
2023-11-09 15:27:36 +0000

    modified

--- infra/lib/CyclopsStack.ts
+++ infra/lib/CyclopsStack.ts
@@ -1,49 +1,46 @@
-import { CfnOutput, DockerImage, Duration, RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
-import { Code, Function, FunctionUrlAuthType, Runtime } from "aws-cdk-lib/aws-lambda";
+import { Duration, RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
+import { Port, Vpc } from "aws-cdk-lib/aws-ec2";
+import { Cluster, ContainerImage, FargateService, FargateTaskDefinition, LogDriver } from "aws-cdk-lib/aws-ecs";
 import { LogGroup, RetentionDays } from "aws-cdk-lib/aws-logs";
 import { Construct } from "constructs";
-import * as os from 'os';
 
 export class CyclopsStack extends Stack {
     constructor(scope: Construct, id: string, props: StackProps) {
         super(scope, id, props);
 
-        const webapp = new Function(this, 'Webapp', {
-            runtime: Runtime.JAVA_17,
-            code: Code.fromAsset(`${__dirname}/../../webapp`, {
-                bundling: {
-                    image: DockerImage.fromBuild(`${__dirname}/../builder`),
-                    volumes: [{
-                        hostPath: `${os.homedir()}/.m2`, containerPath: '/var/maven/.m2'
-                    }, {
-                        hostPath: `${os.homedir()}/.npm`, containerPath: '/.npm'
-                    }],
-                    environment: {
-                        MAVEN_CONFIG: '/var/maven/.m2',
-                    },
-                    command: ['bash', '-c', [
-                        'mvn clean -Duser.home=/var/maven',
-                        'npm --prefix src/main/nuxt3 run build',
-                        'mkdir -p ./target/classes/public',
-                        'cp -R ./src/main/nuxt3/dist/* ./target/classes/public/',
-                        'mvn package -DfinalName=webapp -Duser.home=/var/maven',
-                        'cp ./target/webapp.jar /asset-output'
-                    ].join(' && ')],
-                }
+        const vpc = Vpc.fromLookup(this, 'Vpc', { vpcId: 'vpc-0316bba888afa43c5' });
+        const cluster = Cluster.fromClusterAttributes(vpc, 'Cluster', { clusterName: 'default', vpc, });
+
+        const taskDefinition = new FargateTaskDefinition(this, 'TaskDefinition', {
+            cpu: 512, memoryLimitMiB: 1024,
+        });
+        const main = taskDefinition.addContainer('main', {
+            image: ContainerImage.fromAsset(`${__dirname}/../../webapp`),
+            portMappings: [{ containerPort: 8080 }],
+            logging: LogDriver.awsLogs({
+                streamPrefix: 'Service',
+                logGroup: new LogGroup(cluster, 'LogGroup', {
+                    retention: RetentionDays.ONE_DAY,
+                    removalPolicy: RemovalPolicy.DESTROY,
+                }),
             }),
-            handler: 'com.mahitotsu.cyclops.webapp.Handler::handleRequest',
-            memorySize: 256,
-            timeout: Duration.seconds(10),
-        });
-        new LogGroup(webapp, 'LogGroup', {
-            logGroupName: `/aws/lambda/${webapp.functionName}`,
-            retention: RetentionDays.ONE_DAY,
-            removalPolicy: RemovalPolicy.DESTROY,
-        });
-        const endpoint = webapp.addFunctionUrl({
-            authType: FunctionUrlAuthType.NONE,
+            startTimeout: Duration.seconds(60),
+            stopTimeout: Duration.seconds(10),
+            healthCheck: {
+                command: ['CMD-SHELL', 'test -e application.pid'],
+                interval: Duration.seconds(5),
+                timeout: Duration.seconds(3),
+                retries: 10,
+                startPeriod: Duration.seconds(5),
+            },
         });
 
-        new CfnOutput(this, 'WebappEndpointUrl', { value: endpoint.url });
+        const service = new FargateService(cluster, 'Service', {
+            cluster, taskDefinition, assignPublicIp: true,
+            capacityProviderStrategies: [{ capacityProvider: 'FARGATE_SPOT', weight: 1, base: 0 }],
+            circuitBreaker: { rollback: true }, minHealthyPercent: 0, maxHealthyPercent: 200,
+            desiredCount: 1,
+        });
+        service.connections.allowFromAnyIpv4(Port.tcp(8080));
     }
 }
---
+++ webapp/src/main/java/com/mahitotsu/cyclops/webapp/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.mahitotsu.cyclops.webapp;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
+        return http
+                .sessionManagement(
+                        sessionManager -> sessionManager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user001").password(this.passwordEncoder().encode("pass001")).roles("USER").build());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
