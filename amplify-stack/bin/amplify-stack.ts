8261f091ed15cfe97082cd62cba0da4b924b51fd
akring <akio@mail.com>
2023-11-21 05:57:44 +0000

    clear

--- amplify-stack/bin/amplify-stack.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-import { App } from 'aws-cdk-lib';
-import 'source-map-support/register';
-import { AmplifyStack } from '../lib/AmplifyStack';
-
-const app = new App();
-new AmplifyStack(app, 'AmplifyStack', {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: 'ap-northeast-1' }
-});
