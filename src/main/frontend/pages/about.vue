60aa37881334d1fa4f4fe4f7d917e012c38d1a63
akring <akio@mail.com>
2023-12-09 00:49:46 +0900

    modified

---
+++ src/main/frontend/pages/about.vue
@@ -0,0 +1,6 @@
+<template>
+    <NuxtLink to="/">Home</NuxtLink>
+    <hr />
+    <h1>About</h1>
+    <p>This is an about page.</p>
+</template>
--- src/main/java/com/mahitotsu/cyclops/webapp/Main.java
+++ src/main/java/com/mahitotsu/cyclops/webapp/Main.java
@@ -3,14 +3,12 @@ package com.mahitotsu.cyclops.webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class Main {
 
     public static void main(final String... args) {
         final SpringApplication app = new SpringApplicationBuilder(Main.class).build(args);
-        app.addListeners(new ApplicationPidFileWriter());
         app.run(args);
     }
 }
