3816afc30529e77d2e4f21bbde6716078d48c5fd
akring <akio@mail.com>
2023-12-15 12:12:48 +0000

    clear

--- 01_frontend/pages/index.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <NuxtLink to="/about">About</NuxtLink>
-    <hr />
-    <h1>Home</h1>
-    <p>This is a first page of webapp.</p>
-</template>
--- bin/cyclops.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-import { App } from 'aws-cdk-lib';
-import 'source-map-support/register';
-import { WebappStack } from '../lib/WebappStack';
-
-const app = new App();
-new WebappStack(app, 'WebappStack', {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: 'ap-northeast-1' },
-});
