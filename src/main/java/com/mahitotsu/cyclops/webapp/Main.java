00de56223ecffc19ce8ccca06856559b0b70ee5f
akring <akio@mail.com>
2023-12-19 08:28:09 +0000

    modified

--- src/main/java/com/mahitotsu/cyclops/webapp/Main.java
+++ src/main/java/com/mahitotsu/cyclops/webapp/Main.java
@@ -1,11 +1,75 @@
 package com.mahitotsu.cyclops.webapp;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ConversionServiceFactoryBean;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.core.convert.converter.ConverterFactory;
+import org.springframework.lang.Nullable;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
 public class Main {
     public static void main(final String... args) {
         new SpringApplicationBuilder(Main.class).run(args);
+    }
+
+    @Bean(name = "conversionService")
+    public ConversionServiceFactoryBean conversionServiceFactoryBean() {
+
+        final ObjectMapper mapper = new ObjectMapper();
+
+        final ConversionServiceFactoryBean fb = new ConversionServiceFactoryBean();
+        fb.setConverters(Stream.of(
+                new ObjectToJsonNodeConverter(mapper),
+                new JsonNodeToObjectConverter(mapper))
+                .collect(Collectors.toSet()));
+        return fb;
+    }
+
+    private static class ObjectToJsonNodeConverter implements Converter<Object, JsonNode> {
+
+        private ObjectToJsonNodeConverter(final ObjectMapper mapper) {
+            this.mapper = mapper;
+        }
+
+        private final ObjectMapper mapper;
+
+        @Override
+        @Nullable
+        public JsonNode convert(final Object source) {
+            return this.mapper.valueToTree(source);
+        }
+    }
+
+    private static class JsonNodeToObjectConverter implements ConverterFactory<JsonNode, Object> {
+
+        private JsonNodeToObjectConverter(final ObjectMapper mapper) {
+            this.mapper = mapper;
+        }
+
+        private final ObjectMapper mapper;
+
+        @Override
+        public <T> Converter<JsonNode, T> getConverter(Class<T> targetType) {
+            return new Converter<JsonNode, T>() {
+                @Override
+                @Nullable
+                public T convert(final JsonNode source) {
+                    try {
+                        return JsonNodeToObjectConverter.this.mapper.treeToValue(source, targetType);
+                    } catch (JsonProcessingException e) {
+                        throw new IllegalStateException(e);
+                    }
+                }
+            };
+        }
     }
 }
--- src/main/java/com/mahitotsu/cyclops/webapp/entity/FormEntity.java
+++ src/main/java/com/mahitotsu/cyclops/webapp/entity/FormEntity.java
@@ -5,12 +5,15 @@ import org.hibernate.annotations.Type;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Version;
+import lombok.Data;
 
 @Entity
+@Data
 public class FormEntity {
     
     @Id
@@ -21,5 +24,6 @@ public class FormEntity {
     private long version;
 
     @Type(JsonBinaryType.class)
+    @Column(columnDefinition = "jsonb")
     private JsonNode data;
 }
