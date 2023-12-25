67b76c531100c0bbdefd6e69538fc3b5f5d82fcd
akring <akio@mail.com>
2023-12-25 11:39:03 +0000

    clear

--- src/test/java/com/mahitotsu/cyclops/webapp/TestMain.java
+++
@@ -1,9 +0,0 @@
-package com.mahitotsu.cyclops.webapp;
-
-import org.springframework.boot.SpringApplication;
-
-public class TestMain {
-    public static void main(final String... args) {
-        SpringApplication.from(Main::main).with(TestContainerConfiguration.class).run(args);
-    }
-}
