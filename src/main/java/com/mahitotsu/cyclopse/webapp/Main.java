71ef1fce467aa67e7d2128c45df7c6eb0d85e772
akring <akio@mail.com>
2023-12-27 08:49:43 +0000

    modified

---
+++ src/main/java/com/mahitotsu/cyclopse/webapp/Main.java
@@ -0,0 +1,14 @@
+package com.mahitotsu.cyclopse.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(final String... args) {
+        final SpringApplication app = new SpringApplicationBuilder(Main.class).build(args);
+        app.run(args);
+    }
+}
