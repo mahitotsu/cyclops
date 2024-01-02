43e06b568d7c002e76f52591fb87a58520b56d68
akring <akio@mail.com>
2024-01-02 14:19:56 +0000

    modified

--- src/main/java/com/mahitotsu/cyclops/webapp/data/AbstractEntityBase.java
+++ src/main/java/com/mahitotsu/cyclops/webapp/data/AbstractEntityBase.java
@@ -19,6 +19,9 @@ import jakarta.persistence.PrePersist;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 import jakarta.persistence.Version;
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
@@ -34,34 +37,49 @@ public abstract class AbstractEntityBase implements Serializable {
     @Getter
     @Id
     @Column(name = "_id", unique = true, nullable = false, updatable = false)
+    @NotNull
     private UUID id;
 
     @Getter
     @Version
     @Column(name = "_version", nullable = false)
+    @NotNull
+    @Min(0)
     private Long version;
 
     @Getter
     @CreatedDate
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "_created_date", nullable = false, updatable = false)
+    @NotNull
     private LocalDateTime createdDateTime;
 
     @Getter
     @CreatedBy
     @Column(name = "_created_by", nullable = false, updatable = false)
+    @NotNull
     private String createdBy;
 
     @Getter
     @LastModifiedDate
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "_last_modified_date", nullable = false)
+    @NotNull
     private LocalDateTime lastModifiedDateTime;
 
     @Getter
     @LastModifiedBy
     @Column(name = "_last_modified_by", nullable = false, updatable = false)
+    @NotNull
     private String lastModifiedBy;
+
+    @AssertTrue
+    private boolean isValid() {
+        return (this.createdDateTime != null && this.lastModifiedDateTime != null)
+                ? this.createdDateTime.equals(this.lastModifiedDateTime)
+                        || this.createdDateTime.isBefore(this.lastModifiedDateTime)
+                : true;
+    }
 
     @PrePersist
     private void onPersist() {
--- src/test/java/com/mahitotsu/cyclops/webapp/data/AbstractDataTestBase.java
+++ src/test/java/com/mahitotsu/cyclops/webapp/data/AbstractDataTestBase.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
 @DataJpaTest(properties = """
-        spring.jpa.generate-ddl=true
-        """, showSql = true)
+                spring.jpa.generate-ddl=true
+                """, showSql = true)
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 @Import({ TestContainerConfiguration.class, DataConfiguration.class })
 public class AbstractDataTestBase {
