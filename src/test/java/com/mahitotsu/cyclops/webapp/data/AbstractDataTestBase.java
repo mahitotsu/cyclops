4527f094112c5c21d8e2275cf808352c98b3fbca
akring <akio@mail.com>
2024-01-07 00:22:31 +0900

    clear

--- src/test/java/com/mahitotsu/cyclops/webapp/data/AbstractDataTestBase.java
+++ src/test/java/com/mahitotsu/cyclops/webapp/data/AbstractDataTestBase.java
@@ -2,10 +2,12 @@ package com.mahitotsu.cyclops.webapp.data;
 
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
-import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
-@JdbcTest
+@DataJpaTest(properties = """
+        spring.jpa.generate-ddl=true
+        """, showSql = true)
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 @Import({ DataTestConfiguration.class, DataConfiguration.class })
 public class AbstractDataTestBase {
