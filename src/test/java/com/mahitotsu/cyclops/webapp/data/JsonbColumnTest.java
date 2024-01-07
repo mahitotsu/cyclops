6d63fc987caa40e1edc09e7aacea7aa8bfa86752
akring <akio@mail.com>
2024-01-08 03:36:47 +0900

    modified

--- src/test/java/com/mahitotsu/cyclops/webapp/data/JsonbColumnTest.java
+++ src/test/java/com/mahitotsu/cyclops/webapp/data/JsonbColumnTest.java
@@ -1,7 +1,6 @@
 package com.mahitotsu.cyclops.webapp.data;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
 import java.util.List;
