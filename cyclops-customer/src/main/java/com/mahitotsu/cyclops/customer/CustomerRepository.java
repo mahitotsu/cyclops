d19f6aada59fe5b18e88e78123e5c4a7b0623b34
akring <akio@mail.com>
2024-03-23 17:46:57 +0900

    clear

--- cyclops-customer/src/main/java/com/mahitotsu/cyclops/customer/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mahitotsu.cyclops.customer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {
-
-}
